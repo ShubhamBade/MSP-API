31426b4e9c5155ce30e02ecd95121cf2b4d2ab2c
Shubham <shubhambade3@gmail.com>
2023-04-11 15:56:35 +0530

    updated

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ db.once('open', () => console.log('Connected to MongoDB'));
 // Define user schema
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  location: { type: String, required: true },
+  location: { type: Array, required: true },
   verified: { type: Boolean, required: true },
 });
 
@@ -30,10 +30,27 @@ const userSchema = new mongoose.Schema({
 // Create user model
 const User = mongoose.model('User', userSchema);
 
-
 // Define route to search for users
 app.get('/users', async (req, res) => {
   try {
+
+    const data=  await User.find();
+    console.log("data : "+data.length);
+    if (data==0) {
+      // Add some sample data to the database
+      const sampleData = [
+        { name: 'John Doe', location: 'New York', verified: true },
+        { name: 'Jane Smith', location: 'Los Angeles', verified: false },
+        { name: 'Bob Johnson', location: 'San Francisco', verified: true },
+        { name: 'Alice Lee', location: 'Chicago', verified: false },
+        { name: 'David Chen', location: 'Houston', verified: true },
+      ];
+      User.insertMany(sampleData)
+        .then(() => console.log('Sample data added to database'))
+        .catch((err) => console.error(err));
+
+    }
+
     const searchString = req.query.search ? req.query.search : '';
     const locations = req.query.location ? req.query.location.split(',') : [];
     const verified = req.query.verified === 'true';
@@ -50,20 +67,6 @@ app.get('/users', async (req, res) => {
 
     const count=users.length;
     console.log(count)
-    if (count==0) {
-      // Add some sample data to the database
-      const sampleData = [
-        { name: 'John Doe', location: 'New York', verified: true },
-        { name: 'Jane Smith', location: 'Los Angeles', verified: false },
-        { name: 'Bob Johnson', location: 'San Francisco', verified: true },
-        { name: 'Alice Lee', location: 'Chicago', verified: false },
-        { name: 'David Chen', location: 'Houston', verified: true },
-      ];
-      User.insertMany(sampleData)
-        .then(() => console.log('Sample data added to database'))
-        .catch((err) => console.error(err));
-
-    }
     res.json(users);
   } catch (err) {
     console.error(err);
